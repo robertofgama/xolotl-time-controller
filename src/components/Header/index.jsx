2f3552c65b87629327b91fc4f5e0ba50311b4ac4
Roberto Gama <robertofgama@gmail.com>
2024-03-21 10:46:36 -0300

    ✨ feat: componente header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import { Container } from './styles';
+
+export function Header() {
+  return (
+    <Container>
+      <div className="left" />
+      <div className="center"><h1>Xolotl Timer</h1></div>
+      <div className="right" />
+
+    </Container>
+  );
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.nav`
+  height: 8rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+`;
